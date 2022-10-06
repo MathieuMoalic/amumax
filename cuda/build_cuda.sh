0d860b72095a143911ba3965ac4274bf71d0db27
Mathieu Moalic <matmoa@pm.me>
2022-10-06 12:14:48 +0200

    remove old cc

--- cuda/build_cuda.sh
+++ cuda/build_cuda.sh
@@ -5,7 +5,7 @@ go build -v cuda2go.go
 for file in *.cu; do
     bname=$(basename -s .cu $file)
     echo Compiling $file ...
-    for cc in 50 52 53 60 61 62 70 72 75 80; do
+    for cc in 52 53 60 61 62 70 72 75 80; do
         nvcc -I/opt/cuda/include/ $NVCCFLAGS -arch=compute_$cc -code=sm_$cc $file -o $bname\_$cc.ptx
         ./cuda2go $file
     done
