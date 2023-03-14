b6e984b44d0b61799a9958f4ca6e272fe978fd68
Mathieu Moalic <matmoa@pm.me>
2023-03-14 10:17:15 +0100

    Add file template for GUI instead of inline str

--- engine/gui.go
+++ engine/gui.go
@@ -102,7 +102,7 @@ func (g *guistate) Add(name string, value interface{}) {
 // Once Params/Quants have been declared and added,
 // initialize the GUI Page (pre-renders template) and register http handlers
 func (g *guistate) PrepareServer() {
-	g.Page = gui.NewPage(templText, g)
+	g.Page = gui.NewPage(g)
 	util.SetProgress(GUI.Prog)
 	g.OnAnyEvent(func() {
 		g.incCacheBreaker()
