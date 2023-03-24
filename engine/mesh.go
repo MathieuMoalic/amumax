a759660642eae3af666dc8f1263cc4b1a905236d
Mathieu Moalic <matmoa@pm.me>
2023-03-24 15:15:21 +0100

    Major refactor around chunk saving

--- engine/mesh.go
+++ engine/mesh.go
@@ -9,38 +9,23 @@ import (
 	"github.com/MathieuMoalic/amumax/zarr"
 )
 
-var globalmesh_ data.Mesh // mesh for m and everything that has the same size
-var ZarrMeta zarr.MetaStruct
-
-var chunks Chunks
-var Nx int
-var Ny int
-var Nz int
-var dx float64
-var dy float64
-var dz float64
-var PBCx int
-var PBCy int
-var PBCz int
-
-type Chunk struct {
-	len int
-	nb  int
-}
-
-type Chunks struct {
-	x Chunk
-	y Chunk
-	z Chunk
-	c Chunk
-}
+var (
+	Nx          int
+	Ny          int
+	Nz          int
+	dx          float64
+	dy          float64
+	dz          float64
+	PBCx        int
+	PBCy        int
+	PBCz        int
+	AutoMesh    bool
+	globalmesh_ data.Mesh
+	ZarrMeta    zarr.MetaStruct
+)
 
 func init() {
-	DeclFunc("InitMesh", InitMesh, "")
-	DeclFunc("InitAutoMesh", InitAutoMesh, "")
-	DeclFunc("Chunkx", Chunkx, "")
-	DeclFunc("Chunky", Chunky, "")
-	DeclFunc("Chunkz", Chunkz, "")
+	DeclVar("AutoMesh", &AutoMesh, "")
 	DeclVar("Nx", &Nx, "")
 	DeclVar("Ny", &Ny, "")
 	DeclVar("Nz", &Nz, "")
@@ -138,94 +123,23 @@ func ValidateCellSize() {
 	}
 }
 
-func InitAutoMesh() {
-	InitMeshInner(true)
-}
-
-func InitMesh() {
-	InitMeshInner(false)
-}
-func InitMeshInner(smooth bool) {
-	SetBusy(true)
-	defer SetBusy(false)
-	ValidateGridSize()
-	ValidateCellSize()
-	if smooth {
-		SmoothMesh()
-	}
-	if globalmesh_.Size() == [3]int{0, 0, 0} {
-		globalmesh_ = *data.NewMesh(Nx, Ny, Nz, dx, dy, dz, PBCx, PBCy, PBCz)
-		M.alloc()
-		regions.alloc()
-	} else {
-		panic("Can't resize the mesh in amumax sorry :/")
-	}
-	chunks.Init()
-	ZarrMeta.Init(globalmesh_, OD(), cuda.GPUInfo)
-}
-
-func Chunkx(nb_of_chunks int) {
-	chunks.x.Assign(0, nb_of_chunks)
-}
-func Chunky(nb_of_chunks int) {
-	chunks.y.Assign(1, nb_of_chunks)
-}
-func Chunkz(nb_of_chunks int) {
-	chunks.z.Assign(2, nb_of_chunks)
-}
-
-func (chunks *Chunks) Init() {
-	*chunks = Chunks{
-		Chunk{Nx, 1},
-		Chunk{Ny, 1},
-		Chunk{Nz, 1},
-		Chunk{3, 1},
-	}
-}
-
-func (chunk *Chunk) Assign(N_index, nb_of_chunks int) {
-	name := []string{"Nx", "Ny", "Nz"}[N_index]
-	N := []int{Nx, Ny, Nz}[N_index]
-	checkMesh()
-	if chunk.nb != 1 {
-		util.Fatal("Error: You cannot change the chunking once it's set.")
-	}
-	if nb_of_chunks < 1 || (nb_of_chunks > N) {
-		util.Fatal("Error: The number of chunks must be between 1 and ", name)
-	}
-	new_nb_of_chunks := closestDivisor(N, nb_of_chunks)
-	if new_nb_of_chunks != nb_of_chunks {
-		LogOut("Warning: The number of chunks for", name, "has been automatically resized from", nb_of_chunks, "to", new_nb_of_chunks)
-	}
-	nb_of_chunks = new_nb_of_chunks
-	*chunk = Chunk{N / nb_of_chunks, nb_of_chunks}
-}
-
 func printf(f float64) float32 {
 	return float32(f)
 }
 
-func closestDivisor(N int, D int) int {
-	closest := 0
-	minDist := math.MaxInt32
-	for i := 1; i <= N; i++ {
-		if N%i == 0 {
-			dist := i - D
-			if dist < 0 {
-				dist = -dist
-			}
-			if dist < minDist {
-				minDist = dist
-				closest = i
-			}
-		}
-	}
-	return closest
-}
-
-// check if mesh is set
+// check if mesh is set, otherwise, it creates it
 func checkMesh() {
 	if globalmesh_.Size() == [3]int{0, 0, 0} {
-		util.Fatal("Error: You need to set the mesh first")
+		SetBusy(true)
+		defer SetBusy(false)
+		ValidateGridSize()
+		ValidateCellSize()
+		if AutoMesh {
+			SmoothMesh()
+		}
+		globalmesh_ = *data.NewMesh(Nx, Ny, Nz, dx, dy, dz, PBCx, PBCy, PBCz)
+		M.alloc()
+		regions.alloc()
+		ZarrMeta.Init(globalmesh_, OD(), cuda.GPUInfo)
 	}
 }
