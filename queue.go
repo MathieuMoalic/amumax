f3014d112b0311bdee7676fc5f263c49a5e27c35
Mathieu Moalic <matmoa@pm.me>
2023-04-08 15:35:55 +0200

    Fix mumax queue

--- queue.go
+++ queue.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -158,6 +159,22 @@ func (s *stateTab) PrintTo(w io.Writer) {
 	}
 }
 
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
 func (s *stateTab) RenderHTML(w io.Writer) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
@@ -165,22 +182,42 @@ func (s *stateTab) RenderHTML(w io.Writer) {
 <!DOCTYPE html> <html> <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta http-equiv="refresh" content="1">
-`+engine.CSS+`
+	<style media="all" type="text/css">
+
+		* {
+			box-sizing: border-box;
+		}
+
+		html body {
+			background: rgb(40, 42, 54);
+			color: rgb(248, 248, 242);
+			margin-left: 5%;
+			margin-right: 5%;
+			font-family: sans-serif;
+			font-size: 14px;
+		}
+		table { border-collapse: collapse; }
+		td        { padding: 1px 5px; }
+		hr        { border-style: none; border-top: 1px solid #CCCCCC; }
+		a         { color: #50fa7b; text-decoration: none; }
+		div       { margin-left: 20px; margin-top: 5px; margin-bottom: 20px; }
+		div#footer{ color:gray; font-size:14px; border:none; }
+		.ErrorBox { color: red; font-weight: bold; font-size: 1em; } 
+		.TextBox  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; }
+		textarea  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; color:gray; font-size: 1em; }
+	</style>
 	</head><body>
-	<span style="color:gray; font-weight:bold; font-size:1.5em"> mumax<sup>3</sup> queue status </span><br/>
+	<span style="color:#ffb86c; font-weight:bold; font-size:1.5em"> amumax queue status </span><br/>
 	<hr/>
 	<pre>`)
 
-	hostname := "localhost"
-	hostname, _ = os.Hostname()
 	for _, j := range s.jobs {
 		if j.webAddr != "" {
-			fmt.Fprint(w, `<b>`, j.uid, ` <a href="`, "http://", hostname+j.webAddr, `">`, j.inFile, " ", j.webAddr, "</a></b>\n")
+			fmt.Fprint(w, `<b>`, j.uid, ` <a href="`, "http://", GetLocalIP()+j.webAddr, `">`, j.inFile, " ", j.webAddr, "</a></b>\n")
 		} else {
 			fmt.Fprint(w, j.uid, " ", j.inFile, "\n")
 		}
 	}
-
 	fmt.Fprintln(w, `</pre><hr/></body></html>`)
 }
 
