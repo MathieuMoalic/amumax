e308407f87b1dc0c5b62e8b94e1589ca20953cad
Mathieu Moalic <matmoa@pm.me>
2023-04-27 10:35:03 +0200

    Progress bar width update with magnets

--- zarr/progress_bar.go
+++ zarr/progress_bar.go
@@ -1,34 +1,50 @@
 package zarr
 
 import (
-	"fmt"
+	"os"
 	"strings"
+
+	"github.com/fatih/color"
+	"golang.org/x/term"
 )
 
 type ProgressBar struct {
 	start float64
 	stop  float64
 	last  int
+	out   *color.Color
 }
 
 func (bar *ProgressBar) New(start float64, stop float64) {
 	bar.start = start
 	bar.stop = stop
 	bar.last = 0
+	bar.out = color.New(color.FgGreen)
 }
 
-func (bar *ProgressBar) Update(current float64) {
-	p := int((current-bar.start)/(bar.stop-bar.start)*100) + 1
-	if p > 100 {
-		p = 100
+func (bar *ProgressBar) GetTermWidth() int {
+	width, _, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		width = 30
 	}
-	if p > bar.last {
-		bar.last = p
-		fmt.Printf("\r// [%-50s]%3d%%", strings.Repeat("█", p/2), p)
+	return width
+}
+
+func (bar *ProgressBar) Update(current_time float64) {
+	percentage := int((current_time-bar.start)/(bar.stop-bar.start)*100) + 1
+	if percentage > 100 {
+		percentage = 100
+	}
+	if percentage > bar.last {
+		width := bar.GetTermWidth()
+		bar.last = percentage
+		a := int((width - 4) / 2 * percentage / 100)
+		b := int((width - 4) * (100 - percentage) / 100)
+		bar.out.Print("\r//[" + strings.Repeat("🧲", a) + strings.Repeat(" ", b) + "]")
 	}
 
 }
 
 func (bar *ProgressBar) Finish() {
-	fmt.Printf("\r// [%-50s]%3d%%\n", strings.Repeat("█", 50), 100)
+	bar.out.Println("\r//[" + strings.Repeat("🧲", int(bar.GetTermWidth()-4)/2) + "]")
 }
