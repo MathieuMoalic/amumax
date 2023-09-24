10bb277f0ba49fe60d6f8ec1b76b509deec31343
Mathieu Moalic <matmoa@pm.me>
2023-09-24 20:59:01 +0200

    Fix Cuda

--- cuda/build_cuda.sh
+++ cuda/build_cuda.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 NVCCFLAGS="-std=c++03 -ccbin=/usr/bin/gcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx"
+CC=52
 cd $(dirname -- "$0")
 rm -f *_wrapper.go *.ptx cuda2go
 go build -v cuda2go.go 
 for file in *.cu; do
     bname=$(basename -s .cu $file)
     echo Compiling $file ...
-    for cc in 52; do
-        nvcc -I/opt/cuda/include/ $NVCCFLAGS -arch=compute_$cc -code=sm_$cc $file -o $bname\_$cc.ptx
-        ./cuda2go $file
-    done
+    nvcc -I/opt/cuda/include/ $NVCCFLAGS -arch=compute_$CC -code=sm_$CC $file -o $bname\_$CC.ptx
+    ./cuda2go $file
 done
