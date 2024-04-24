8e9ccca3a2b9664106155c25680a7b69c4a7a9cb
Mathieu Moalic <matmoa@pm.me>
2024-04-24 08:42:48 +0200

    Remove some assertion

--- cuda/madd.go
+++ cuda/madd.go
@@ -10,7 +10,7 @@ import (
 func Mul(dst, a, b *data.Slice) {
 	N := dst.Len()
 	nComp := dst.NComp()
-	util.Assert(a.Len() == N && a.NComp() == nComp && b.Len() == N && b.NComp() == nComp)
+	// util.Assert(a.Len() == N && a.NComp() == nComp && b.Len() == N && b.NComp() == nComp)
 	cfg := make1DConf(N)
 	for c := 0; c < nComp; c++ {
 		k_mul_async(dst.DevPtr(c), a.DevPtr(c), b.DevPtr(c), N, cfg)
