ebbde398324b65ad2b5f0969025b8614cd940e08
Mathieu Moalic <matmoa@pm.me>
2024-05-27 10:47:04 +0200

    Format Mesh section

--- frontend/src/lib/Mesh.svelte
+++ frontend/src/lib/Mesh.svelte
@@ -1,53 +1,69 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { meshState } from './api';
-	onMount(() => {
-		// console.log('meshState', $meshState);
-	});
+	function formatFloat(value: number): string {
+		return value.toFixed(3);
+	}
 </script>
 
 <section>
 	<h2>Mesh</h2>
-	<div>
-		<table>
-			<tbody>
-				<tr>
-					<td>gridsize: </td>
-					<td><input bind:value={$meshState.Nx} /></td>
-					<td> × <input bind:value={$meshState.Ny} /></td>
-					<td> × <input bind:value={$meshState.Nz} /></td>
-					<td> cells </td>
-				</tr>
-				<tr>
-					<td>cellsize: </td>
-					<td><input bind:value={$meshState.dx} /> </td>
-					<td> × <input bind:value={$meshState.dy} /></td>
-					<td> × <input bind:value={$meshState.dz} /></td>
-					<td> m<sup>3</sup> </td>
-				</tr>
-				<tr>
-					<td>PBC: </td>
-					<td><input bind:value={$meshState.PBCx} /> </td>
-					<td> × <input bind:value={$meshState.PBCy} /></td>
-					<td> × <input bind:value={$meshState.PBCz} /></td>
-					<td> repetitions </td>
-				</tr>
-				<tr>
-					<td>worldsize:</td>
-					<td><span>{$meshState.Tx} </span> </td>
-					<td> × <span> {$meshState.Ty} </span></td>
-					<td> × <span> {$meshState.Tz} </span></td>
-					<td> m<sup>3</sup> </td>
-				</tr>
-			</tbody>
-		</table>
+	<div class="grid-container">
+		<div class="header">Nx:</div>
+		<div><input placeholder=" {$meshState.Nx}" /></div>
+		<div>×</div>
+		<div class="header">Ny:</div>
+		<div><input placeholder=" {$meshState.Ny}" /></div>
+		<div>×</div>
+		<div class="header">Nz:</div>
+		<div><input placeholder=" {$meshState.Nz}" /></div>
+		<div>cells</div>
 
-		<button>update</button>
+		<div class="header">dx:</div>
+		<div><input placeholder=" {$meshState.dx.toExponential(3)}" /></div>
+		<div>×</div>
+		<div class="header">dy:</div>
+		<div><input placeholder=" {$meshState.dy.toExponential(3)}" /></div>
+		<div>×</div>
+		<div class="header">dz:</div>
+		<div><input placeholder=" {$meshState.dz.toExponential(3)}" /></div>
+		<div>m<sup>3</sup></div>
+
+		<div class="header">PBCx:</div>
+		<div><input placeholder=" {$meshState.PBCx}" /></div>
+		<div>×</div>
+		<div class="header">PBCy:</div>
+		<div><input placeholder=" {$meshState.PBCy}" /></div>
+		<div>×</div>
+		<div class="header">PBCz:</div>
+		<div><input placeholder=" {$meshState.PBCz}" /></div>
+		<div>repetitions</div>
+
+		<div class="header">Tx:</div>
+		<div><span>{$meshState.Tx.toExponential(3)}</span></div>
+		<div>×</div>
+		<div class="header">Ty:</div>
+		<div><span>{$meshState.Ty.toExponential(3)}</span></div>
+		<div>×</div>
+		<div class="header">Tz:</div>
+		<div><span>{$meshState.Tz.toExponential(3)}</span></div>
+		<div>m<sup>3</sup></div>
 	</div>
 </section>
 
 <style>
-	section {
-		grid-area: mesh;
+	.grid-container {
+		display: grid;
+		grid-template-columns: auto auto auto auto auto auto auto auto auto;
+		gap: 10px;
+		align-items: center;
+		padding: 10px;
+	}
+	input {
+		width: 100%;
+		box-sizing: border-box;
+	}
+	.header {
+		text-align: right;
+		padding-right: 10px;
 	}
 </style>
