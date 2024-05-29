b93e72d1512653cf4c4be227ec8cf3a55880fe5c
Mathieu Moalic <matmoa@pm.me>
2024-05-29 13:01:57 +0200

    Sending message through websockets finally works

--- api/websocket.go
+++ api/websocket.go
@@ -3,30 +3,54 @@ package api
 import (
 	"time"
 
+	"github.com/MathieuMoalic/amumax/engine"
 	"github.com/MathieuMoalic/amumax/util"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/net/websocket"
 )
 
+var (
+	webSocketState WebSocketState
+)
+
+type WebSocketState struct {
+	LastStep int
+}
+
 // WebSocket handler for engine state updates
 func websocketEntrypoint(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
+		util.Log("WebSocket client connected: ", ws.RemoteAddr().String(), "->", ws.LocalAddr().String())
 		defer ws.Close()
-		util.Log("WebSocket client connected")
+
+		// Send initial state when the client connects to the WebSocket
+		sendEngineState(ws)
+		sendDisplayVectorField(ws)
+
 		for {
-			// Create new engine state
-			engineState := NewEngineState()
-
-			// Send engine state to the WebSocket client
-			err := websocket.JSON.Send(ws, engineState)
-			if err != nil {
-				util.Log("Error sending engine state via WebSocket:", err)
-				break
+			if engine.NSteps != webSocketState.LastStep {
+				sendEngineState(ws)
+				sendDisplayVectorField(ws)
+				webSocketState.LastStep = engine.NSteps
 			}
-
-			// Wait before sending the next state update
-			time.Sleep(1 * time.Second) // Adjust the interval as needed
+			time.Sleep(1 * time.Second)
 		}
 	}).ServeHTTP(c.Response(), c.Request())
 	return nil
+}
+
+func sendEngineState(ws *websocket.Conn) {
+	engineState := NewEngineState()
+	err := websocket.JSON.Send(ws, engineState)
+	if err != nil {
+		util.LogErr("Error sending engine state via WebSocket:", err)
+	}
+}
+
+func sendDisplayVectorField(ws *websocket.Conn) {
+	engine.InjectAndWait(engine.GetVectorField)
+	err := websocket.Message.Send(ws, engine.DisplayVectorField)
+	if err != nil {
+		util.LogErr("Error sending binary data via WebSocket:", err)
+	}
 }
--- frontend/src/lib/websocket.ts
+++ frontend/src/lib/websocket.ts
@@ -1,40 +1,89 @@
 import { type EngineState } from "./types";
 import { get } from "svelte/store";
-import { headerState, consoleState, solverState, meshState, parametersState, tablePlotState, paused, refreshImage, refreshTablePlot } from "./api";
+import { headerState, consoleState, solverState, meshState, parametersState, tablePlotState, paused, displayData } from "./api";
 import { redraw } from "./tablePlot";
+import { update, parsingTime, init } from "./display";
+import { type VectorField } from "./types";
 
 let ws: WebSocket;
-let messages: any[] = [];
-let input = "";
 
-// Connect to WebSocket
 export function wsConnect() {
     ws = new WebSocket('ws://localhost:5001/ws');
 
     ws.onopen = () => {
-        console.log('Connected to the server');
+        // console.log('Connected to the server');
     };
 
     ws.onerror = (error) => {
         // console.error('WebSocket Error:', error);
     };
 
-    ws.onmessage = (e) => {
-        messages = [...messages, e.data];
-        parseMessage(e.data);
-        refreshImage();
-        refreshTablePlot();
+    ws.onmessage = async (e) => {
+        if (typeof e.data === 'string') {
+            try {
+                parseJsonMessage(e.data);
+            } catch (error) {
+                console.error('Error parsing JSON message:', error);
+            }
+        } else if (e.data instanceof Blob) {
+            try {
+                if (get(displayData).length === 0) {
+                    await parseBinaryMessage(e.data);
+                    init()
+                } else {
+                    parseBinaryMessage(e.data);
+                    update();
+                }
+            } catch (error) {
+                console.error('Error parsing Binary message:', error);
+            }
+        }
     };
 
     ws.onclose = (e) => {
-        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-        setTimeout(() => {
-            wsConnect();
-        }, 1000);
+        console.log('Socket is closed.');
+        // console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+        // setTimeout(() => {
+        //     wsConnect();
+        // }, 1000);
     };
 }
 
-function parseMessage(message: string) {
+async function parseBinaryMessage(data: Blob) {
+    const startTime = performance.now(); // Start timing
+
+    const arrayBuffer = await blobToArrayBuffer(data);
+    displayData.set(parseVectorField(arrayBuffer));
+
+    const endTime = performance.now(); // End timing
+    parsingTime.set(endTime - startTime);
+}
+
+function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as ArrayBuffer);
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(blob);
+    });
+}
+
+function parseVectorField(arrayBuffer: ArrayBuffer): VectorField {
+    const float32Array = new Float32Array(arrayBuffer);
+    const vectors: VectorField = [];
+
+    for (let i = 0; i < float32Array.length; i += 3) {
+        vectors.push({
+            x: float32Array[i],
+            y: float32Array[i + 1],
+            z: float32Array[i + 2]
+        });
+    }
+
+    return vectors;
+}
+
+function parseJsonMessage(message: string) {
     let engineState: EngineState = JSON.parse(message);
     headerState.set(engineState.header);
     solverState.set(engineState.solver);
