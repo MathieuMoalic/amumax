7be93687b07761a3726009a69dcc20a9ff5806cc
Mathieu Moalic <matmoa@pm.me>
2024-06-19 11:16:22 +0200

    Reorganize api functions

--- frontend/src/api/outgoing/preview.ts
+++ frontend/src/api/outgoing/preview.ts
@@ -3,25 +3,27 @@ import { get } from "svelte/store";
 import { baseURL } from "$api/websocket";
 
 export interface Preview {
-    Quantity: string;
-    Component: number;
-    Layer: number;
+    quantity: string;
+    component: string;
+    layer: number;
+    maxPoints: number;
 }
-
 export const preview = writable<Preview>({
-    Quantity: 'm',
-    Component: 0,
-    Layer: 0,
+    quantity: 'm',
+    component: 'All',
+    layer: 0,
+    maxPoints: 1000,
 });
 
 export async function postPreview() {
-    let value = get(preview);
+
+    let data = get(preview);
     const response = await fetch(`${get(baseURL)}/preview`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ value })
+        body: JSON.stringify({ ...data })
     });
     if (!response.ok) {
         throw new Error('Failed to post preview');
