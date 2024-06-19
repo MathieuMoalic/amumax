65a15c1283b2c4489d2732eeed5a6adc45ca0850
Mathieu Moalic <matmoa@pm.me>
2024-06-19 11:16:51 +0200

    Add string to int converter to preview input

--- frontend/src/lib/preview/Preview.svelte
+++ frontend/src/lib/preview/Preview.svelte
@@ -2,31 +2,37 @@
 	import quantities from './quantities';
 	import { meshState } from '$api/incoming/mesh';
 	import { preview } from '$api/outgoing/preview';
-	import { onMount } from 'svelte';
-	import { update, display } from '$api/incoming/preview';
+	import { display } from './plot-vector-field';
+	import { postPreview } from '$api/outgoing/preview';
 
-	onMount(() => {});
+	let maxPointsField = '1000';
+
+	function onMaxPointsFieldChange() {
+		$preview.maxPoints = parseInt(maxPointsField);
+		postPreview();
+	}
 </script>
 
 <section>
 	<h2>Preview</h2>
 	<p>
 		Quantity:
-		<select bind:value={$preview.Quantity} on:change={update}>
+		<select bind:value={$preview.quantity} on:change={postPreview}>
 			{#each quantities as quantity}
 				<option value={quantity}>{quantity}</option>
 			{/each}
 		</select>
-		<select bind:value={$preview.Component}>
+		<select bind:value={$preview.component} on:change={postPreview}>
 			{#each ['All', 'x', 'y', 'z'] as choice}
 				<option value={choice}>{choice}</option>
 			{/each}
 		</select>
 		Z-layer: 1
-		<input type="range" min="0" max={$meshState.Nz - 1} bind:value={$preview.Layer} />
-		{$preview.Layer}
-		<button on:click={update}>Refresh</button>
+		<input type="range" min="0" max={$meshState.Nz - 1} bind:value={$preview.layer} />
+		{$preview.layer}
+		<button on:click={postPreview}>Refresh</button>
 	</p>
+	Max Points:<input bind:value={maxPointsField} on:change={onMaxPointsFieldChange} />
 	<div id="container"></div>
 	Parsing time: {$display?.parsingTime} ms
 </section>
