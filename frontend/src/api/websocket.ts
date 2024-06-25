a3b251ec8c1aa932d9e57d4d0476da3f4d2cdb34
Mathieu Moalic <matmoa@pm.me>
2024-06-25 11:45:46 +0200

    Change from json ws messages to msgpack

--- frontend/src/api/websocket.ts
+++ frontend/src/api/websocket.ts
@@ -1,32 +1,4 @@
 import msgpack from 'msgpack-lite';
-
-export let ws: WebSocket;
-
-export function initializeWebSocket(ws: WebSocket) {
-    ws = new WebSocket('ws://localhost:5001/ws');
-    ws.binaryType = 'arraybuffer';
-
-    ws.onmessage = function (event) {
-        const msg = msgpack.decode(new Uint8Array(event.data));
-
-        const engineState = msg.engine_state as EngineState;
-        // headerState.set(engineState.header);
-        // solverState.set(engineState.solver);
-        // consoleState.set(engineState.console);
-        // meshState.set(engineState.mesh);
-        // parametersState.set(engineState.parameters);
-        // tablePlotState.set(engineState.tablePlot);
-        // redraw();
-
-        displayData.set(parseVectorField(msg.preview_buffer))
-
-        console.log(engineState, get(displayData));
-    };
-}
-// const ws = new WebSocket('ws://localhost:5001/ws');
-
-
-
 import { get, writable } from "svelte/store";
 import { redraw } from "../lib/table-plot/tablePlot";
 import { update, init, display } from "$lib/preview/plot-vector-field"
@@ -40,7 +12,7 @@ import { type Parameters, parametersState } from "./incoming/parameters";
 import { type TablePlot, tablePlotState } from "./incoming/table-plot";
 
 export const baseURL = writable('http://localhost:5001');
-
+export let ws: WebSocket;
 
 export interface EngineState {
     header: Header;
@@ -51,71 +23,40 @@ export interface EngineState {
     tablePlot: TablePlot;
 }
 
-// let ws: WebSocket;
-// export function wsConnect() {
-//     ws = new WebSocket('ws://localhost:5001/ws');
+export function initializeWebSocket(ws: WebSocket) {
+    ws = new WebSocket('ws://localhost:5001/ws');
+    ws.binaryType = 'arraybuffer';
 
-//     ws.onopen = () => {
-//         // console.log('Connected to the server');
-//     };
+    ws.onmessage = function (event) {
+        const startTime = performance.now(); // Start timing
+        const msg = msgpack.decode(new Uint8Array(event.data));
 
-//     ws.onerror = (error) => {
-//         // console.error('WebSocket Error:', error);
-//     };
+        const engineState = msg.engine_state as EngineState;
+        headerState.set(engineState.header);
+        solverState.set(engineState.solver);
+        consoleState.set(engineState.console);
+        meshState.set(engineState.mesh);
+        parametersState.set(engineState.parameters);
+        tablePlotState.set(engineState.tablePlot);
+        redraw();
 
-//     ws.onmessage = async (e) => {
-//         const startTime = performance.now(); // Start timing
-
-//         if (typeof e.data === 'string') {
-//             try {
-//                 parseJsonMessage(e.data);
-//             } catch (error) {
-//                 console.error('Error parsing JSON message:', error);
-//             }
-//         } else if (e.data instanceof Blob) {
-//             try {
-//                 if (get(displayData).length === 0) {
-//                     await parseBinaryMessage(e.data);
-//                     init()
-//                 } else {
-//                     await parseBinaryMessage(e.data);
-//                     update();
-//                 }
-//             } catch (error) {
-//                 console.error('Error parsing Binary message:', error);
-//             }
-//         }
-//         const endTime = performance.now(); // End timing
-//         const parsingTime = endTime - startTime;
-//         display.update(currentDisplay => {
-//             if (currentDisplay) {
-//                 return { ...currentDisplay, parsingTime: parsingTime };
-//             }
-//             return currentDisplay;
-//         });
-//     };
-
-//     ws.onclose = (e) => {
-//         console.log('Socket is closed.');
-//         // console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-//         // setTimeout(() => {
-//         //     wsConnect();
-//         // }, 1000);
-//     };
-// }
-
-async function parseBinaryMessage(data: Blob) {
-    const arrayBuffer = await blobToArrayBuffer(data);
-    displayData.set(parseVectorField(arrayBuffer));
-}
-
-function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer> {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result as ArrayBuffer);
-        reader.onerror = reject;
-        reader.readAsArrayBuffer(blob);
-    });
+        if (get(displayData).length === 0) {
+            displayData.set(parseVectorField(msg.preview_buffer))
+            init()
+        } else {
+            displayData.set(parseVectorField(msg.preview_buffer))
+            update();
+        }
+        console.log(engineState, get(displayData));
+        const endTime = performance.now(); // End timing
+        const parsingTime = endTime - startTime;
+        display.update(currentDisplay => {
+            if (currentDisplay) {
+                return { ...currentDisplay, parsingTime: parsingTime };
+            }
+            return currentDisplay;
+        });
+    };
 }
 
 function parseVectorField(arrayBuffer: ArrayBuffer): VectorField {
@@ -131,16 +72,4 @@ function parseVectorField(arrayBuffer: ArrayBuffer): VectorField {
     }
 
     return vectors;
-}
-
-function parseEngineStateMessage(message: string) {
-    let engineState: EngineState = JSON.parse(message);
-    headerState.set(engineState.header);
-    solverState.set(engineState.solver);
-    consoleState.set(engineState.console);
-    meshState.set(engineState.mesh);
-    parametersState.set(engineState.parameters);
-    tablePlotState.set(engineState.tablePlot);
-    // paused.set(get(headerState).status === 'Paused');
-    redraw()
 }
