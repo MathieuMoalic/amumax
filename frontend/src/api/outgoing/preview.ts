3d34bb2897c9f08a146ec84bcf8dcc9ee8264e0e
Mathieu Moalic <matmoa@pm.me>
2024-07-09 15:47:33 +0200

    cleanup backend post api

--- frontend/src/api/outgoing/preview.ts
+++ frontend/src/api/outgoing/preview.ts
@@ -1,21 +1,23 @@
 import { previewState } from "$api/incoming/preview";
 import { post } from "$api/post";
+import { clearDisplay, display } from "$lib/preview/plot-vector-field";
 import { get } from "svelte/store";
 
 export function postComponent() {
-    post('preview-component', get(previewState).component);
+    post('preview-component', { component: get(previewState).component });
 }
 
 export function postQuantity() {
-    post('preview-quantity', get(previewState).quantity);
+    post('preview-quantity', { quantity: get(previewState).quantity });
 }
 
 export function postLayer() {
-    post('preview-layer', get(previewState).layer);
+    post('preview-layer', { layer: get(previewState).layer });
 }
 
 export function postMaxPoints() {
-    post('preview-maxpoints', get(previewState).maxPoints);
+    clearDisplay();
+    post('preview-maxpoints', { maxPoints: get(previewState).maxPoints });
 }
 
 export function postRefresh() {
