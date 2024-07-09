fa478517d2a91eca720c1dfdce5cfd2ec58ee610
Mathieu Moalic <matmoa@pm.me>
2024-07-09 15:47:33 +0200

    cleanup backend post api

--- frontend/src/api/post.ts
+++ frontend/src/api/post.ts
@@ -8,7 +8,7 @@ export async function post(endpoint: string, data: any) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ data })
+        body: JSON.stringify({ ...data })
     });
     if (!response.ok) {
         throw new Error(`Failed to post ${endpoint}`);
