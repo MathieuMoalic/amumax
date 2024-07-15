d431d21cc69010cb824e357203183eb25acf5bbf
Mathieu Moalic <matmoa@pm.me>
2024-07-15 17:00:33 +0200

    Add static files to binary

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: '../api/static',
+			assets: '../api/static',
 			fallback: undefined,
 			precompress: false,
 			strict: true
