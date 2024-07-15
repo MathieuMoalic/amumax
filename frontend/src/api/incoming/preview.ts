927b2d45f6752d13f360dee79ef26b0b28d9c93f
Mathieu Moalic <matmoa@pm.me>
2024-07-15 16:58:04 +0200

    Add Min,Max, and Units to the preview state

--- frontend/src/api/incoming/preview.ts
+++ frontend/src/api/incoming/preview.ts
@@ -5,20 +5,26 @@ export type ScalarField = Array<number>;
 
 export interface Preview {
     quantity: string;
+    unit: string;
     component: string;
     layer: number;
     maxPoints: number;
     dimensions: [number, number, number];
     type: string;
     buffer: Uint8Array;
+    min: number;
+    max: number;
 }
 
 export const previewState = writable<Preview>({
     quantity: 'm',
+    unit: '',
     component: 'All',
     layer: 0,
     maxPoints: 1000,
     dimensions: [0, 0, 0],
     type: 'vector',
     buffer: new Uint8Array(0),
+    min: 0,
+    max: 0
 });
