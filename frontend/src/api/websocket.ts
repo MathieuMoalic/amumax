3ab3c1509d815f63b1d3742780d7a7063e610886
Mathieu Moalic <matmoa@pm.me>
2024-07-23 11:31:57 +0200

    Fix dev/prod console logging

--- frontend/src/api/websocket.ts
+++ frontend/src/api/websocket.ts
@@ -24,6 +24,7 @@ export function initializeWebSocket() {
         };
 
         ws.onmessage = function (event) {
+            console.log('WebSocket message received');
             parseMsgpack(event.data);
             ws?.send('ok');
         };
