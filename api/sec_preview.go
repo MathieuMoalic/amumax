b735adf1528585e9023c97b8a49119a48dfdcb39
Mathieu Moalic <matmoa@pm.me>
2024-08-13 12:52:13 +0200

    Refactor preview api

--- api/sec_preview.go
+++ api/sec_preview.go
@@ -1,47 +1,239 @@
 package api
 
 import (
+	"math"
 	"net/http"
 
+	"github.com/MathieuMoalic/amumax/cuda"
+	"github.com/MathieuMoalic/amumax/data"
 	"github.com/MathieuMoalic/amumax/engine"
 	"github.com/MathieuMoalic/amumax/util"
 	"github.com/labstack/echo/v4"
 )
 
-type Preview struct {
-	Quantity   string  `msgpack:"quantity"`
-	Unit       string  `msgpack:"unit"`
-	Component  string  `msgpack:"component"`
-	Layer      int     `msgpack:"layer"`
-	MaxPoints  int     `msgpack:"maxPoints"`
-	Dimensions [3]int  `msgpack:"dimensions"`
-	Type       string  `msgpack:"type"`
-	Buffer     *[]byte `msgpack:"buffer"`
-	Min        float32 `msgpack:"min"`
-	Max        float32 `msgpack:"max"`
-}
+var preview Preview
 
-func newPreview() *Preview {
-	return &Preview{
-		Quantity:   engine.NameOf(bps.Quantity),
-		Unit:       engine.UnitOf(bps.Quantity),
-		Component:  compIndexToString(bps.Component),
-		Layer:      bps.Layer,
-		MaxPoints:  bps.MaxPoints,
-		Dimensions: bps.Dimensions,
-		Type:       bps.Type,
-		Buffer:     &bps.Buffer,
-		Min:        bps.Min,
-		Max:        bps.Max,
+func init() {
+	preview = Preview{
+		Quantity:             "m",
+		Component:            "All",
+		Layer:                0,
+		MaxPoints:            10000,
+		Dimensions:           [3]int{0, 0, 0},
+		Type:                 "3D",
+		VectorFieldValues:    nil,
+		VectorFieldPositions: nil,
+		ScalarField:          nil,
+		Min:                  0,
+		Max:                  0,
+		Refresh:              true,
 	}
 }
 
-func updateType() {
-	isVectorField := bps.Quantity.NComp() == 3 && bps.Component == -1
-	if isVectorField {
-		bps.Type = "vector"
+type Preview struct {
+	Quantity             string               `msgpack:"quantity"`
+	Unit                 string               `msgpack:"unit"`
+	Component            string               `msgpack:"component"`
+	Layer                int                  `msgpack:"layer"`
+	MaxPoints            int                  `msgpack:"maxPoints"`
+	Dimensions           [3]int               `msgpack:"dimensions"`
+	Type                 string               `msgpack:"type"`
+	VectorFieldValues    []map[string]float32 `msgpack:"vectorFieldValues"`
+	VectorFieldPositions []map[string]int     `msgpack:"vectorFieldPositions"`
+	ScalarField          [][3]float32         `msgpack:"scalarField"`
+	Min                  float32              `msgpack:"min"`
+	Max                  float32              `msgpack:"max"`
+	Refresh              bool                 `msgpack:"refresh"`
+}
+
+func (p *Preview) GetQuantity() engine.Quantity {
+	quantity, exists := engine.Quantities[p.Quantity]
+	if !exists {
+		util.LogErr("Quantity not found:", p.Quantity)
+	}
+	return quantity
+}
+
+func (p *Preview) GetComponent() int {
+	return compStringToIndex(p.Component)
+}
+
+func (p *Preview) Update() {
+	engine.InjectAndWait(p.UpdateQuantityBuffer)
+}
+
+func (p *Preview) UpdateQuantityBuffer() {
+	nComp := 1
+	if p.Type == "3D" {
+		nComp = 3
+	}
+
+	p.ScaleDimensions()
+
+	GPU_in := engine.ValueOf(p.GetQuantity())
+	defer cuda.Recycle(GPU_in)
+	CPU_out := data.NewSlice(nComp, p.Dimensions)
+	GPU_out := cuda.NewSlice(1, p.Dimensions)
+	defer GPU_out.Free()
+
+	if p.Type == "3D" {
+		for c := 0; c < nComp; c++ {
+			cuda.Resize(GPU_out, GPU_in.Comp(c), p.Layer)
+			data.Copy(CPU_out.Comp(c), GPU_out)
+		}
+		p.normalizeVectors(CPU_out)
+		p.UpdateVectorField(CPU_out.Vectors())
 	} else {
-		bps.Type = "scalar"
+		if p.GetQuantity().NComp() == 3 {
+			cuda.Resize(GPU_out, GPU_in.Comp(p.GetComponent()), p.Layer)
+			data.Copy(CPU_out.Comp(0), GPU_out)
+		} else {
+			cuda.Resize(GPU_out, GPU_in.Comp(0), p.Layer)
+			data.Copy(CPU_out.Comp(0), GPU_out)
+		}
+		p.UpdateScalarField(CPU_out.Scalars())
+	}
+}
+
+func (p *Preview) normalizeVectors(f *data.Slice) {
+	a := f.Vectors()
+	maxnorm := 0.
+	for i := range a[0] {
+		for j := range a[0][i] {
+			for k := range a[0][i][j] {
+
+				x, y, z := a[0][i][j][k], a[1][i][j][k], a[2][i][j][k]
+				norm := math.Sqrt(float64(x*x + y*y + z*z))
+				if norm > maxnorm {
+					maxnorm = norm
+				}
+
+			}
+		}
+	}
+	factor := float32(1 / maxnorm)
+
+	for i := range a[0] {
+		for j := range a[0][i] {
+			for k := range a[0][i][j] {
+				a[0][i][j][k] *= factor
+				a[1][i][j][k] *= factor
+				a[2][i][j][k] *= factor
+
+			}
+		}
+	}
+}
+
+func (p *Preview) UpdateVectorField(vectorField [3][][][]float32) {
+	// Calculate the total number of elements
+	yLen := len(vectorField[0][0])
+	xLen := len(vectorField[0][0][0])
+
+	// Create a slice to hold the array of {x, y, z} objects
+	var valArray []map[string]float32
+	var posArray []map[string]int
+
+	for posx := 0; posx < xLen; posx++ {
+		for posy := 0; posy < yLen; posy++ {
+			valx := vectorField[0][0][posy][posx]
+			valy := vectorField[1][0][posy][posx]
+			valz := vectorField[2][0][posy][posx]
+			if valx == 0 && valy == 0 && valz == 0 {
+				continue
+			}
+			posArray = append(posArray,
+				map[string]int{
+					"x": posx,
+					"y": posy,
+					"z": 0,
+				})
+			valArray = append(valArray,
+				map[string]float32{
+					"x": valx,
+					"y": valy,
+					"z": valz,
+				})
+		}
+	}
+	p.VectorFieldPositions = posArray
+	p.VectorFieldValues = valArray
+	p.ScalarField = nil
+}
+
+func (p *Preview) UpdateScalarField(scalarField [][][]float32) {
+	yLen := len(scalarField[0][0])
+	xLen := len(scalarField[0])
+	min, max := scalarField[0][0][0], scalarField[0][0][0]
+	var valArray [][3]float32
+	for posx := 0; posx < xLen; posx++ {
+		for posy := 0; posy < yLen; posy++ {
+			val := scalarField[0][posy][posx]
+			if val == 0 {
+				continue
+			}
+			if val < min {
+				min = val
+			}
+			if val > max {
+				max = val
+			}
+			valArray = append(valArray, [3]float32{float32(posx), float32(posy), val})
+		}
+	}
+	p.Min = min
+	p.Max = max
+	p.ScalarField = valArray
+	p.VectorFieldValues = nil
+	p.VectorFieldPositions = nil
+}
+
+// ScaleDimensions scales down the image size until the number of points are < MaxVectors
+func (p *Preview) ScaleDimensions() {
+	originalSize := engine.MeshOf(p.GetQuantity()).Size()
+	width, height := float32(originalSize[0]), float32(originalSize[1])
+	points := width * height
+
+	// Calculate the scaling factor
+	for points >= float32(p.MaxPoints) {
+		width = width / 1.1
+		height = height / 1.1
+		points = width * height
+	}
+	p.Dimensions = [3]int{int(width), int(height), 1}
+}
+
+func compStringToIndex(comp string) int {
+	switch comp {
+	case "x":
+		return 0
+	case "y":
+		return 1
+	case "z":
+		return 2
+	case "All":
+		return -1
+	}
+	util.Fatal("Invalid component string")
+	return -2
+}
+
+func newPreview() *Preview {
+	preview.Update()
+	return &preview
+}
+
+func updatePreviewType() {
+	var fieldType string
+	isVectorField := preview.GetQuantity().NComp() == 3 && preview.GetComponent() == -1
+	if isVectorField {
+		fieldType = "3D"
+	} else {
+		fieldType = "2D"
+	}
+	if fieldType != preview.Type {
+		preview.Type = fieldType
+		preview.Refresh = true
 	}
 }
 
@@ -54,8 +246,9 @@ func postPreviewComponent(c echo.Context) error {
 		util.LogErr(err)
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
 	}
-	bps.Component = compStringToIndex(req.Component)
-	updateType()
+	preview.Component = req.Component
+	preview.Refresh = true
+	updatePreviewType()
 	broadcastEngineState()
 	return c.JSON(http.StatusOK, nil)
 }
@@ -69,12 +262,12 @@ func postPreviewQuantity(c echo.Context) error {
 		util.LogErr(err)
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
 	}
-	quantity, exists := engine.Quantities[req.Quantity]
+	_, exists := engine.Quantities[req.Quantity]
 	if !exists {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Quantity not found"})
 	}
-	bps.Quantity = quantity
-	updateType()
+	preview.Quantity = req.Quantity
+	updatePreviewType()
 	broadcastEngineState()
 	return c.JSON(http.StatusOK, nil)
 }
@@ -88,7 +281,8 @@ func postPreviewLayer(c echo.Context) error {
 		util.LogErr(err)
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
 	}
-	bps.Layer = req.Layer
+	preview.Layer = req.Layer
+	preview.Refresh = true
 	broadcastEngineState()
 	return c.JSON(http.StatusOK, nil)
 }
@@ -102,7 +296,8 @@ func postPreviewMaxPoints(c echo.Context) error {
 		util.LogErr(err)
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
 	}
-	bps.MaxPoints = req.MaxPoints
+	preview.MaxPoints = req.MaxPoints
+	preview.Refresh = true
 	broadcastEngineState()
 	return c.JSON(http.StatusOK, nil)
 }
