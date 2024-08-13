86ce1671e191c4a1bf594f8823eeefbeb2876715
Mathieu Moalic <matmoa@pm.me>
2024-08-13 16:59:14 +0200

    Add a message if the table is empty

--- frontend/src/lib/table-plot/TablePlot.svelte
+++ frontend/src/lib/table-plot/TablePlot.svelte
@@ -10,28 +10,34 @@
 
 <section>
 	<h2>Table Plot</h2>
-	<p>
-		Auto Save Interval: <input
-			type="number"
-			bind:value={$tablePlotState.autoSaveInterval}
-			on:change={postAutoSaveInterval}
-		/> s
-	</p>
-	<b>
-		x:
-		<select bind:value={$tablePlotState.xColumn} on:change={postXColumn}>
-			{#each $tablePlotState.columns as q}
-				<option value={q}>{q}</option>
-			{/each}
-		</select>
-		y:
-		<select bind:value={$tablePlotState.yColumn} on:change={postYColumn}>
-			{#each $tablePlotState.columns as q}
-				<option value={q}>{q}</option>
-			{/each}
-		</select>
-	</b>
-	<div id="table-plot"></div>
+	{#if $tablePlotState.data.length === 0}
+		<div class="msg">
+			<p>No table data, use <code>TableSave()</code> to save data.</p>
+		</div>
+	{:else}
+		<p>
+			Auto Save Interval: <input
+				type="number"
+				bind:value={$tablePlotState.autoSaveInterval}
+				on:change={postAutoSaveInterval}
+			/> s
+		</p>
+		<b>
+			x:
+			<select bind:value={$tablePlotState.xColumn} on:change={postXColumn}>
+				{#each $tablePlotState.columns as q}
+					<option value={q}>{q}</option>
+				{/each}
+			</select>
+			y:
+			<select bind:value={$tablePlotState.yColumn} on:change={postYColumn}>
+				{#each $tablePlotState.columns as q}
+					<option value={q}>{q}</option>
+				{/each}
+			</select>
+		</b>
+		<div id="table-plot"></div>
+	{/if}
 </section>
 
 <style>
@@ -41,5 +47,8 @@
 	#table-plot {
 		width: 100%;
 		height: 500px;
+	}
+	.msg {
+		color: #888;
 	}
 </style>
