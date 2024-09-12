fb61f5e79a3cad20706799149cfca909eb8f8a47
Mathieu Moalic <matmoa@pm.me>
2024-09-12 09:09:00 +0200

    flowbite: solver mostly done

---
+++ frontend/src/lib/alerts/Alert.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Toast } from 'flowbite-svelte';
+	import { alert } from './alert';
+	import { ExclamationCircleSolid } from 'flowbite-svelte-icons';
+</script>
+
+{#if $alert !== ''}
+	<div class="fixed left-3/4 top-10 z-50 gap-10">
+		<Toast color="red">
+			<svelte:fragment slot="icon">
+				<ExclamationCircleSolid class="h-5 w-5" />
+				<span class="sr-only">Warning icon</span>
+			</svelte:fragment>
+			{$alert}
+		</Toast>
+	</div>
+{/if}
---
+++ frontend/src/lib/preview/QuantityDropdown.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Button, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
+	import { ChevronDownOutline, ChevronRightOutline } from 'flowbite-svelte-icons';
+	import { previewState as p } from '$api/incoming/preview';
+	import quantities from './quantities';
+</script>
+
+<Button
+	>Dropdown button<ChevronDownOutline class="ms-2 h-6 w-6 text-white dark:text-white" /></Button
+>
+<Dropdown>
+	<DropdownItem>Dashboard</DropdownItem>
+	<DropdownItem class="flex items-center justify-between">
+		Dropdown<ChevronRightOutline class="text-primary-700 ms-2 h-6 w-6 dark:text-white" />
+	</DropdownItem>
+	<Dropdown placement="right-start">
+		<DropdownItem>Overview</DropdownItem>
+		<DropdownItem>My downloads</DropdownItem>
+		<DropdownItem>Billing</DropdownItem>
+	</Dropdown>
+	<DropdownItem>Earnings</DropdownItem>
+	<DropdownItem slot="footer">Sign out</DropdownItem>
+</Dropdown>
+
+<!-- <Select bind:value={$p.quantity} on:change={postQuantity}>
+    {#each quantities as quantity}
+        <option value={quantity}>{quantity}</option>
+    {/each}
+</Select> -->
