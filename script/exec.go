9b8a72ca98d20820b6971609cc5bdc428cc0e2c6
Mathieu Moalic <matmoa@pm.me>
2024-09-26 10:21:08 +0200

    Fixed misnamed variable

--- script/exec.go
+++ script/exec.go
@@ -18,16 +18,17 @@ func (w *World) MustExec(src string) {
 
 // Eval with panic on error.
 func (w *World) MustEval(src string) interface{} {
-	Expr := w.MustCompileExpr(src)
-	return Expr.Eval()
+	Exprr := w.MustCompileExpr(src)
+	return Exprr.Eval()
 }
 
 // Eval compiles and evaluates src, which must be an expression, and returns the result(s). E.g.:
-// 	world.Eval("1+1")      // returns 2, nil
+//
+//	world.Eval("1+1")      // returns 2, nil
 func (w *World) Eval(src string) (ret interface{}, err error) {
-	Expr, err := w.CompileExpr(src)
+	Exprr, err := w.CompileExpr(src)
 	if err != nil {
 		return nil, err
 	}
-	return Expr.Eval(), nil
+	return Exprr.Eval(), nil
 }
