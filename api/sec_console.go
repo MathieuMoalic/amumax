6c71625fa33133f50e3ee92a16cf85eff2c68b96
Mathieu Moalic <matmoa@pm.me>
2024-09-26 13:57:15 +0200

    chore: Prevent execution of "RunShell" command through the WebUI

--- api/sec_console.go
+++ api/sec_console.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/MathieuMoalic/amumax/engine"
 	"github.com/MathieuMoalic/amumax/util"
@@ -27,6 +28,12 @@ func postConsoleCommand(c echo.Context) error {
 	if err := c.Bind(req); err != nil {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
 	}
+
+	// Check if "RunShell" is in the command, case-insensitive
+	if strings.Contains(strings.ToLower(req.Command), "runshell(") {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "RunShell command not allowed through the WebUI"})
+	}
+
 	engine.InjectAndWait(func() { engine.EvalTryRecover(req.Command) })
 	broadcastEngineState()
 	return c.JSON(http.StatusOK, "")
