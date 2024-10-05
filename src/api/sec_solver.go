4536b15b4ecbfbebfbd98f7916608d7f192d6711
Mathieu Moalic <matmoa@pm.me>
2024-10-05 09:36:45 +0200

    api is partially without global variables

--- src/api/sec_solver.go
+++ src/api/sec_solver.go
@@ -1,174 +1,185 @@
 package api
 
-import (
-	"fmt"
-	"net/http"
-	"strconv"
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"strconv"
 
-	"github.com/MathieuMoalic/amumax/src/engine"
-	"github.com/labstack/echo/v4"
-)
+// 	"github.com/MathieuMoalic/amumax/src/engine"
+// 	"github.com/labstack/echo/v4"
+// )
 
-type Solver struct {
-	Type       string  `msgpack:"type"`
-	Steps      int     `msgpack:"steps"`
-	Time       float64 `msgpack:"time"`
-	Dt         float64 `msgpack:"dt"`
-	ErrPerStep float64 `msgpack:"errPerStep"`
-	MaxTorque  float64 `msgpack:"maxTorque"`
-	Fixdt      float64 `msgpack:"fixdt"`
-	Mindt      float64 `msgpack:"mindt"`
-	Maxdt      float64 `msgpack:"maxdt"`
-	Maxerr     float64 `msgpack:"maxerr"`
-}
+// type Solver struct {
+// 	Type       string  `msgpack:"type"`
+// 	Steps      int     `msgpack:"steps"`
+// 	Time       float64 `msgpack:"time"`
+// 	Dt         float64 `msgpack:"dt"`
+// 	ErrPerStep float64 `msgpack:"errPerStep"`
+// 	MaxTorque  float64 `msgpack:"maxTorque"`
+// 	Fixdt      float64 `msgpack:"fixdt"`
+// 	Mindt      float64 `msgpack:"mindt"`
+// 	Maxdt      float64 `msgpack:"maxdt"`
+// 	Maxerr     float64 `msgpack:"maxerr"`
+// }
 
-var (
-	Solvertypes = map[string]int{"bw_euler": -1, "euler": 1, "heun": 2, "rk23": 3, "rk4": 4, "rk45": 5, "rkf56": 6}
-	Solvernames = map[int]string{-1: "bw_euler", 1: "euler", 2: "heun", 3: "rk23", 4: "rk4", 5: "rk45", 6: "rkf56"}
-)
+// func newSolver() *Solver {
+// 	return &Solver{
+// 		Type:       getSolverName(engine.Solvertype),
+// 		Steps:      engine.NSteps,
+// 		Time:       engine.Time,
+// 		Dt:         engine.Dt_si,
+// 		ErrPerStep: engine.LastErr,
+// 		MaxTorque:  engine.LastTorque,
+// 		Fixdt:      engine.FixDt,
+// 		Mindt:      engine.MinDt,
+// 		Maxdt:      engine.MaxDt,
+// 		Maxerr:     engine.MaxErr,
+// 	}
+// }
 
-func newSolver() *Solver {
-	return &Solver{
-		Type:       Solvernames[engine.Solvertype],
-		Steps:      engine.NSteps,
-		Time:       engine.Time,
-		Dt:         engine.Dt_si,
-		ErrPerStep: engine.LastErr,
-		MaxTorque:  engine.LastTorque,
-		Fixdt:      engine.FixDt,
-		Mindt:      engine.MinDt,
-		Maxdt:      engine.MaxDt,
-		Maxerr:     engine.MaxErr,
-	}
-}
+// func getSolverType(typeStr string) int {
+// 	solvertypes := map[string]int{"bw_euler": -1, "euler": 1, "heun": 2, "rk23": 3, "rk4": 4, "rk45": 5, "rkf56": 6}
+// 	if solver, ok := solvertypes[typeStr]; ok {
+// 		return solver
+// 	}
+// 	return 0
+// }
 
-func postSolverType(c echo.Context) error {
-	type Response struct {
-		Type string `msgpack:"type"`
-	}
+// func getSolverName(solver int) string {
+// 	solvernames := map[int]string{-1: "bw_euler", 1: "euler", 2: "heun", 3: "rk23", 4: "rk4", 5: "rk45", 6: "rkf56"}
+// 	if name, ok := solvernames[solver]; ok {
+// 		return name
+// 	}
+// 	return ""
+// }
 
-	res := new(Response)
-	if err := c.Bind(res); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
-	engine.InjectAndWait(func() {
-		solver := Solvertypes[res.Type]
+// func postSolverType(c echo.Context) error {
+// 	type Response struct {
+// 		Type string `msgpack:"type"`
+// 	}
 
-		// euler must have fixed time step
-		if solver == engine.EULER && engine.FixDt == 0 {
-			engine.EvalTryRecover("FixDt = 1e-15")
-		}
-		if solver == engine.BACKWARD_EULER && engine.FixDt == 0 {
-			engine.EvalTryRecover("FixDt = 1e-13")
-		}
+// 	res := new(Response)
+// 	if err := c.Bind(res); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
+// 	engine.InjectAndWait(func() {
+// 		solver := getSolverType(res.Type)
 
-		engine.EvalTryRecover(fmt.Sprint("SetSolver(", solver, ")"))
-	})
+// 		// euler must have fixed time step
+// 		if solver == engine.EULER && engine.FixDt == 0 {
+// 			engine.EvalTryRecover("FixDt = 1e-15")
+// 		}
+// 		if solver == engine.BACKWARD_EULER && engine.FixDt == 0 {
+// 			engine.EvalTryRecover("FixDt = 1e-13")
+// 		}
 
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, engine.Solvertype)
-}
+// 		engine.EvalTryRecover(fmt.Sprint("SetSolver(", solver, ")"))
+// 	})
 
-func postSolverRun(c echo.Context) error {
-	type Request struct {
-		Runtime float64 `msgpack:"runtime"`
-	}
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, engine.Solvertype)
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
-	engine.Break()
-	engine.InjectAndWait(func() { engine.EvalTryRecover("Run(" + strconv.FormatFloat(req.Runtime, 'f', -1, 64) + ")") })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// func postSolverRun(c echo.Context) error {
+// 	type Request struct {
+// 		Runtime float64 `msgpack:"runtime"`
+// 	}
 
-func postSolverSteps(c echo.Context) error {
-	type Request struct {
-		Steps int `msgpack:"steps"`
-	}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
+// 	engine.Break()
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("Run(" + strconv.FormatFloat(req.Runtime, 'f', -1, 64) + ")") })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
+// func postSolverSteps(c echo.Context) error {
+// 	type Request struct {
+// 		Steps int `msgpack:"steps"`
+// 	}
 
-	engine.Break()
-	engine.InjectAndWait(func() { engine.EvalTryRecover("Steps(" + strconv.Itoa(req.Steps) + ")") })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
 
-func postSolverRelax(c echo.Context) error {
-	engine.Break()
-	engine.InjectAndWait(func() { engine.EvalTryRecover("Relax()") })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	engine.Break()
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("Steps(" + strconv.Itoa(req.Steps) + ")") })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-func postSolverBreak(c echo.Context) error {
-	engine.Break()
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// func postSolverRelax(c echo.Context) error {
+// 	engine.Break()
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("Relax()") })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-func postSolverFixDt(c echo.Context) error {
-	type Request struct {
-		Fixdt float64 `msgpack:"fixdt"`
-	}
+// func postSolverBreak(c echo.Context) error {
+// 	engine.Break()
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
+// func postSolverFixDt(c echo.Context) error {
+// 	type Request struct {
+// 		Fixdt float64 `msgpack:"fixdt"`
+// 	}
 
-	engine.InjectAndWait(func() { engine.EvalTryRecover("FixDt = " + strconv.FormatFloat(req.Fixdt, 'f', -1, 64)) })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
 
-func postSolverMinDt(c echo.Context) error {
-	type Request struct {
-		Mindt float64 `msgpack:"mindt"`
-	}
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("FixDt = " + strconv.FormatFloat(req.Fixdt, 'f', -1, 64)) })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
+// func postSolverMinDt(c echo.Context) error {
+// 	type Request struct {
+// 		Mindt float64 `msgpack:"mindt"`
+// 	}
 
-	engine.InjectAndWait(func() { engine.EvalTryRecover("MinDt = " + strconv.FormatFloat(req.Mindt, 'f', -1, 64)) })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
 
-func postSolverMaxDt(c echo.Context) error {
-	type Request struct {
-		Maxdt float64 `msgpack:"maxdt"`
-	}
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("MinDt = " + strconv.FormatFloat(req.Mindt, 'f', -1, 64)) })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
+// func postSolverMaxDt(c echo.Context) error {
+// 	type Request struct {
+// 		Maxdt float64 `msgpack:"maxdt"`
+// 	}
 
-	engine.InjectAndWait(func() { engine.EvalTryRecover("MaxDt = " + strconv.FormatFloat(req.Maxdt, 'f', -1, 64)) })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
 
-func postSolverMaxErr(c echo.Context) error {
-	type Request struct {
-		Maxerr float64 `msgpack:"maxerr"`
-	}
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("MaxDt = " + strconv.FormatFloat(req.Maxdt, 'f', -1, 64)) })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
 
-	req := new(Request)
-	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
-	}
+// func postSolverMaxErr(c echo.Context) error {
+// 	type Request struct {
+// 		Maxerr float64 `msgpack:"maxerr"`
+// 	}
 
-	engine.InjectAndWait(func() { engine.EvalTryRecover("MaxErr = " + strconv.FormatFloat(req.Maxerr, 'f', -1, 64)) })
-	broadcastEngineState()
-	return c.JSON(http.StatusOK, "")
-}
+// 	req := new(Request)
+// 	if err := c.Bind(req); err != nil {
+// 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request payload"})
+// 	}
+
+// 	engine.InjectAndWait(func() { engine.EvalTryRecover("MaxErr = " + strconv.FormatFloat(req.Maxerr, 'f', -1, 64)) })
+// 	broadcastEngineState()
+// 	return c.JSON(http.StatusOK, "")
+// }
