aba3e640c4528871ba31d481ba49f11a59568b3a
Mathieu Moalic <matmoa@pm.me>
2024-10-23 09:46:32 +0200

    Update github hash for the release of 2024.10.23

--- flake.nix
+++ flake.nix
@@ -89,13 +89,13 @@
       };
 
     #################### RELEASE ########################
-    releaseVersion = "2024.10.17"; # Set the version for the Release build
+    releaseVersion = "2024.10.23"; # Set the version for the Release build
 
     ReleaseSrc = pkgs.fetchFromGitHub {
       owner = "MathieuMoalic";
       repo = "amumax";
       rev = releaseVersion;
-      hash = pkgs.lib.fakeHash;
+      hash = "sha256-lGCsbnmThXGTIP72cScE4c24uKMtNMcMvMtLoKVJa+I=";
     };
 
     ReleaseFrontend = buildFrontend {
