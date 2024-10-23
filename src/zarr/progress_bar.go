845c051c62b20026cd386166517f414668fe9bec
Mathieu Moalic <matmoa@pm.me>
2024-10-23 20:14:18 +0200

    Fix visual bugs on progress bar

--- src/zarr/progress_bar.go
+++ src/zarr/progress_bar.go
@@ -1,6 +1,7 @@
 package zarr
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -9,50 +10,88 @@ import (
 )
 
 type ProgressBar struct {
-	start   float64
-	stop    float64
-	last    int
-	out     *color.Color
-	enabled bool
-	symbol  string
+	start     float64
+	stop      float64
+	last      int
+	out       *color.Color
+	enabled   bool
+	symbol    string
+	minLength int
 }
 
-func (bar *ProgressBar) New(start float64, stop float64, symbol string, enabled bool) {
-	bar.start = start
-	bar.stop = stop
-	bar.last = 0
-	bar.out = color.New(color.FgGreen)
-	bar.enabled = enabled
-	bar.symbol = symbol
+func NewProgressBar(start float64, stop float64, symbol string, enabled bool) *ProgressBar {
+	return &ProgressBar{
+		start:     start,
+		stop:      stop,
+		last:      -1,
+		out:       color.New(color.FgGreen),
+		enabled:   enabled,
+		symbol:    symbol,
+		minLength: 10,
+	}
 }
 
-func (bar *ProgressBar) GetTermWidth() int {
-	width, _, err := term.GetSize(int(os.Stdin.Fd()))
-	if err != nil {
-		width = 30
+func (bar *ProgressBar) getTermWidth() int {
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil || width <= 0 {
+		width = bar.minLength // Default width if unable to get terminal size
 	}
 	return width
 }
 
-func (bar *ProgressBar) Update(current_time float64) {
-	if bar.enabled {
-		percentage := int((current_time-bar.start)/(bar.stop-bar.start)*100) + 1
-		if percentage > 100 {
-			percentage = 100
-		}
-		if percentage > bar.last {
-			width := bar.GetTermWidth()
-			bar.last = percentage
-			a := int((width - 4) / 2 * percentage / 100)
-			b := int((width - 4) * (100 - percentage) / 100)
-			bar.out.Print("\r//[" + strings.Repeat(bar.symbol, a) + strings.Repeat(" ", b) + "]")
-		}
+func (bar *ProgressBar) Update(currentTime float64) {
+	if !bar.enabled {
+		return
 	}
 
+	percentage := int((currentTime - bar.start) / (bar.stop - bar.start) * 100)
+	if percentage > 100 {
+		percentage = 100
+	}
+	if percentage < 0 {
+		percentage = 0
+	}
+
+	if percentage > bar.last {
+		width := bar.getTermWidth()
+		bar.last = percentage
+
+		// Adjust the bar length to account for fixed characters
+		barLength := width - bar.minLength // For "[", "]", percentage, and spaces
+		if barLength < bar.minLength {
+			barLength = bar.minLength // Minimum bar length
+		}
+
+		filledLength := int(float64(barLength) * float64(percentage) / 100.0)
+		emptyLength := barLength - filledLength
+
+		progressBar := fmt.Sprintf("\r[%s%s] %3d%%",
+			strings.Repeat(bar.symbol, filledLength),
+			strings.Repeat(" ", emptyLength),
+			percentage)
+
+		// Clear the line
+		fmt.Print("\r" + strings.Repeat(" ", width))
+		// Write the progress bar
+		bar.out.Print(progressBar)
+	}
 }
 
 func (bar *ProgressBar) Finish() {
 	if bar.enabled {
-		bar.out.Println("\r//[" + strings.Repeat(bar.symbol, int(bar.GetTermWidth()-4)/2) + "]")
+		width := bar.getTermWidth()
+
+		barLength := width - bar.minLength
+		if barLength < bar.minLength {
+			barLength = bar.minLength
+		}
+
+		progressBar := fmt.Sprintf("\r[%s] 100%%\n",
+			strings.Repeat(bar.symbol, barLength))
+
+		// Clear the line
+		fmt.Print("\r" + strings.Repeat(" ", width))
+		// Write the progress bar
+		bar.out.Print(progressBar)
 	}
 }
