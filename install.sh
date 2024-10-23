a693a2a6a0b7fac51eb724cd24d3704e3da755cd
Mathieu Moalic <matmoa@pm.me>
2024-10-23 13:58:13 +0200

    Make install script nicer

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
 
+# Check for necessary commands
+for cmd in curl tar xz; do
+  if ! command -v $cmd > /dev/null 2>&1; then
+    echo "Error: '$cmd' is not installed. Please install it and try again." >&2
+    exit 1
+  fi
+done
+
 DEST=$1
 
+# Prompt for installation path if not provided
 if [ -z "$DEST" ]; then
   printf "Where to install amumax? [Default=$HOME/.local/bin]: "
   read DEST
@@ -13,30 +22,40 @@ fi
 
 mkdir -p "$DEST"
 DEST=$(realpath "$DEST")
+
+# Warn if DEST is not in PATH
 case ":$PATH:" in
   *:"$DEST":*) ;;
-  *) echo && echo " !!! WARNING !!! '$DEST' not in PATH !" && echo >&2 ;;
+  *) 
+    echo && echo " !!! WARNING !!! '$DEST' not in PATH!"
+    echo "Consider adding '$DEST' to your PATH." >&2
+    ;;
 esac
 
+# Download and install amumax
 cd $DEST
-echo Downloading amumax from https://github.com/mathieumoalic ...
-curl -Ls https://github.com/mathieumoalic/amumax/releases/latest/download/amumax > amumax
+echo "Downloading amumax from GitHub..."
+curl -Ls https://github.com/mathieumoalic/amumax/releases/latest/download/amumax -o amumax
 
-echo Downloading libcufft.so.10 from https://developer.download.nvidia.com/compute/cuda/redist/ ...
-curl -Ls https://developer.download.nvidia.com/compute/cuda/redist/libcufft/linux-x86_64/libcufft-linux-x86_64-10.9.0.58-archive.tar.xz > tmp
-tar -xvf tmp > /dev/null
+# Download and extract necessary libraries
+echo "Downloading and extracting libcufft.so.10..."
+curl -Ls https://developer.download.nvidia.com/compute/cuda/redist/libcufft/linux-x86_64/libcufft-linux-x86_64-10.9.0.58-archive.tar.xz -o tmp
+tar -xJf tmp > /dev/null
 cp -L libcufft-linux-x86_64-10.9.0.58-archive/lib/libcufft.so.10 .
 
-echo Downloading libcurand.so.10 from https://developer.download.nvidia.com/compute/cuda/redist/ ...
-curl -Ls https://developer.download.nvidia.com/compute/cuda/redist/libcurand/linux-x86_64/libcurand-linux-x86_64-10.3.6.39-archive.tar.xz > tmp
-tar -xvf tmp > /dev/null
+echo "Downloading and extracting libcurand.so.10..."
+curl -Ls https://developer.download.nvidia.com/compute/cuda/redist/libcurand/linux-x86_64/libcurand-linux-x86_64-10.3.6.39-archive.tar.xz -o tmp
+tar -xJf tmp > /dev/null
 cp -L libcurand-linux-x86_64-10.3.6.39-archive/lib/libcurand.so.10 .
 
+# Clean up
 rm -r libcufft-linux-x86_64-10.9.0.58-archive
 rm -r libcurand-linux-x86_64-10.3.6.39-archive
 rm tmp
 
+# Make amumax executable
 echo "Setting amumax as executable"
 chmod +x amumax
 
-echo "You can now use 'amumax'"
+# Completion message
+echo "Installation complete. You can now use 'amumax'."
