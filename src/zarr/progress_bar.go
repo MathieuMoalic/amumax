3325f23b310433aacb6cd52a1a880316290eab79
Mathieu Moalic <matmoa@pm.me>
2024-10-25 10:17:29 +0200

    Progress bar is nicer now

--- src/zarr/progress_bar.go
+++ src/zarr/progress_bar.go
@@ -4,45 +4,72 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
 	"golang.org/x/term"
 )
 
 type ProgressBar struct {
-	start     float64
-	stop      float64
-	last      int
-	out       *color.Color
-	enabled   bool
-	symbol    string
-	minLength int
+	start       float64
+	stop        float64
+	last        int
+	out         *color.Color
+	enabled     bool
+	symbol      string
+	symbolWidth int
+	minLength   int
+	lastUpdate  time.Time
 }
 
 func NewProgressBar(start float64, stop float64, symbol string, enabled bool) *ProgressBar {
 	return &ProgressBar{
-		start:     start,
-		stop:      stop,
-		last:      -1,
-		out:       color.New(color.FgGreen),
-		enabled:   enabled,
-		symbol:    symbol,
-		minLength: 10,
+		start:       start,
+		stop:        stop,
+		last:        -1,
+		out:         color.New(color.FgGreen),
+		enabled:     enabled,
+		symbol:      symbol,
+		symbolWidth: 2, // Width of the symbol in characters
+		minLength:   1, // Minimum bar length in symbols
+		lastUpdate:  time.Now(),
 	}
 }
 
 func (bar *ProgressBar) getTermWidth() int {
 	width, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil || width <= 0 {
-		width = bar.minLength // Default width if unable to get terminal size
+		width = 80 // Default width if unable to get terminal size
 	}
 	return width
+}
+
+func (bar *ProgressBar) calculateDimensions(percentage int) (int, int) {
+	width := bar.getTermWidth()
+	fixedLength := 1 + 1 + 1 + 4 // '[', ']', space, '100%'
+	availableWidth := width - fixedLength
+	availableWidth -= availableWidth % bar.symbolWidth
+	barLength := availableWidth / bar.symbolWidth
+	if barLength < bar.minLength {
+		barLength = bar.minLength
+	}
+	filledLength := int(float64(barLength) * float64(percentage) / 100.0)
+	if filledLength > barLength {
+		filledLength = barLength
+	}
+	emptyLength := barLength - filledLength
+	return emptyLength, filledLength
 }
 
 func (bar *ProgressBar) Update(currentTime float64) {
 	if !bar.enabled {
 		return
 	}
+	now := time.Now()
+	if now.Sub(bar.lastUpdate) < time.Millisecond*100 {
+		return
+	}
+	bar.lastUpdate = now
 
 	percentage := int((currentTime - bar.start) / (bar.stop - bar.start) * 100)
 	if percentage > 100 {
@@ -54,24 +81,25 @@ func (bar *ProgressBar) Update(currentTime float64) {
 
 	if percentage > bar.last {
 		width := bar.getTermWidth()
+		if width < 10 {
+			// Don't display the progress bar if the terminal is too small
+			return
+		}
 		bar.last = percentage
 
-		// Adjust the bar length to account for fixed characters
-		barLength := width - bar.minLength // For "[", "]", percentage, and spaces
-		if barLength < bar.minLength {
-			barLength = bar.minLength // Minimum bar length
-		}
+		emptyLength, filledLength := bar.calculateDimensions(percentage)
 
-		filledLength := int(float64(barLength) * float64(percentage) / 100.0)
-		emptyLength := barLength - filledLength
+		filledSymbols := strings.Repeat(bar.symbol, filledLength)
+		emptySymbols := strings.Repeat("  ", emptyLength)
 
 		progressBar := fmt.Sprintf("\r[%s%s] %3d%%",
-			strings.Repeat(bar.symbol, filledLength),
-			strings.Repeat(" ", emptyLength),
+			filledSymbols,
+			emptySymbols,
 			percentage)
 
 		// Clear the line
-		fmt.Print("\r" + strings.Repeat(" ", width))
+		bar.out.Print("\r\033[K")
+
 		// Write the progress bar
 		bar.out.Print(progressBar)
 	}
@@ -79,18 +107,16 @@ func (bar *ProgressBar) Update(currentTime float64) {
 
 func (bar *ProgressBar) Finish() {
 	if bar.enabled {
-		width := bar.getTermWidth()
+		_, filledLength := bar.calculateDimensions(100)
 
-		barLength := width - bar.minLength
-		if barLength < bar.minLength {
-			barLength = bar.minLength
-		}
+		filledSymbols := strings.Repeat(bar.symbol, filledLength)
 
 		progressBar := fmt.Sprintf("\r[%s] 100%%\n",
-			strings.Repeat(bar.symbol, barLength))
+			filledSymbols)
 
 		// Clear the line
-		fmt.Print("\r" + strings.Repeat(" ", width))
+		bar.out.Print("\r\033[K")
+
 		// Write the progress bar
 		bar.out.Print(progressBar)
 	}
