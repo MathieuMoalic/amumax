cf9b5f3ef94b9e6253e7bb9047a7337694f1aaf9
Mathieu Moalic <matmoa@pm.me>
2024-11-06 13:48:37 +0100

    console UI auto scrolling

--- frontend/src/lib/Console.svelte
+++ frontend/src/lib/Console.svelte
@@ -4,20 +4,42 @@
 
 	import Prism from 'prismjs';
 	import 'prismjs/components/prism-go'; // Ensure the import path is correct
+	import { onMount, tick } from 'svelte';
 
 	let command = '';
+	let codeDiv: HTMLDivElement | null = null;
+
+	// Handle Enter key to submit commands
 	function handleKeydown(event: KeyboardEvent) {
 		if (event.key === 'Enter') {
 			postCommand(command);
 			event.preventDefault(); // Prevent the default action to avoid form submission or newline in input
 		}
 	}
+	async function scrollDown() {
+		if (codeDiv) {
+			// Wait for the DOM to update (e.g. after a new line in hist is added)
+			await tick();
+			const isAtBottom = codeDiv.scrollTop + codeDiv.clientHeight >= codeDiv.scrollHeight - 100;
+			if (isAtBottom) {
+				codeDiv.scrollBy(0, codeDiv.scrollHeight);
+			}
+		}
+	}
+
+	$: {
+		$consoleState.hist; // Trigger the reactive statement when hist changes
+		scrollDown();
+	}
+	onMount(() => {
+		scrollDown();
+	});
 </script>
 
 <section>
 	<h2 class="mb-4 text-2xl font-semibold">Console</h2>
 	<div class="flex flex-col gap-2">
-		<div class="code">
+		<div class="code" bind:this={codeDiv}>
 			{@html Prism.highlight($consoleState.hist, Prism.languages['go'], 'go')}
 		</div>
 		<br />
