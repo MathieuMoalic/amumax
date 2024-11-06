71901178e829fef1ef1748807dab5fa3fdb73539
Mathieu Moalic <matmoa@pm.me>
2024-11-06 15:28:15 +0100

    Add version to header

--- frontend/src/api/incoming/header.ts
+++ frontend/src/api/incoming/header.ts
@@ -3,10 +3,11 @@ import { writable } from "svelte/store";
 export interface Header {
     path: string;
     status: string;
-
+    version: string;
 }
 
 export const headerState = writable({
     path: '',
-    status: ''
+    status: '',
+    version: ''
 });
