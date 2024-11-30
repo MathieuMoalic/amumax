6412c80c3e55fa7e1856f05fe48b3192efa41a26
Mathieu Moalic <matmoa@pm.me>
2024-11-30 09:40:28 +0100

    Put table in its own package

--- src/table/table.go
+++ src/table/table.go
@@ -1,4 +1,4 @@
-package engine
+package table
 
 import (
 	"bufio"
@@ -8,13 +8,19 @@ import (
 	"os"
 	"sync"
 
+	"github.com/MathieuMoalic/amumax/src/fsutil"
+	"github.com/MathieuMoalic/amumax/src/log"
 	"github.com/MathieuMoalic/amumax/src/quantity"
+	"github.com/MathieuMoalic/amumax/src/script"
+	"github.com/MathieuMoalic/amumax/src/solver"
 	"github.com/MathieuMoalic/amumax/src/utils"
 )
 
-// the table is kept in RAM and used for the API
-type table struct {
-	e              *engineState
+// the Table is kept in RAM and used for the API
+type Table struct {
+	log            *log.Logs
+	fs             *fsutil.FileSystem
+	solver         *solver.Solver
 	quantities     []quantity.Quantity
 	columns        []column
 	Data           map[string][]float64 `json:"data"`
@@ -34,9 +40,11 @@ type column struct {
 }
 
 // NewTable creates a new empty table, does not save it to disk
-func newTable(e *engineState) *table {
-	t := &table{
-		e:              e,
+func NewTable(solver *solver.Solver, log *log.Logs, fs *fsutil.FileSystem, script *script.ScriptParser) *Table {
+	t := &Table{
+		solver:         solver,
+		log:            log,
+		fs:             fs,
 		quantities:     []quantity.Quantity{},
 		columns:        []column{},
 		Data:           make(map[string][]float64),
@@ -47,15 +55,15 @@ func newTable(e *engineState) *table {
 		initialized:    false,
 		lastSavedHash:  "",
 	}
-	e.script.RegisterFunction("TableAutoSave", t.tableAutoSave)
-	e.script.RegisterFunction("TableAdd", t.tableAdd)
-	e.script.RegisterFunction("TableAddAs", t.tableAddAs)
-	e.script.RegisterFunction("TableSave", t.tableSave)
+	script.RegisterFunction("TableAutoSave", t.tableAutoSave)
+	script.RegisterFunction("TableAdd", t.tableAdd)
+	script.RegisterFunction("TableAddAs", t.tableAddAs)
+	script.RegisterFunction("TableSave", t.tableSave)
 	return t
 }
 
 // generateHash creates a hash based on the current Step and column names.
-func (ts *table) generateHash() string {
+func (ts *Table) generateHash() string {
 	hash := sha256.New()
 	fmt.Fprint(hash, ts.Step) // Include the Step in the hash
 	for _, col := range ts.columns {
@@ -65,20 +73,20 @@ func (ts *table) generateHash() string {
 }
 
 // InitTable creates default columns and saves them to disk
-func (ts *table) initTable() {
-	err := ts.e.fs.Remove("table")
-	ts.e.log.PanicIfError(err)
-	err = ts.e.fs.CreateZarrGroup("table/")
-	ts.e.log.PanicIfError(err)
+func (ts *Table) initTable() {
+	err := ts.fs.Remove("table")
+	ts.log.PanicIfError(err)
+	err = ts.fs.CreateZarrGroup("table/")
+	ts.log.PanicIfError(err)
 	ts.addColumn("step", "")
 	ts.addColumn("t", "s")
 	ts.initialized = true
 }
 
-func (ts *table) writeToBuffer() {
+func (ts *Table) writeToBuffer() {
 	buf := []float64{}
 	buf = append(buf, float64(ts.Step))
-	buf = append(buf, ts.e.solver.Time)
+	buf = append(buf, ts.solver.Time)
 	for _, q := range ts.quantities {
 		buf = append(buf, q.Average()...)
 	}
@@ -93,7 +101,7 @@ func (ts *table) writeToBuffer() {
 		// Write directly to the buffered writer
 		_, err := ts.columns[i].writer.Write(data)
 		if err != nil {
-			ts.e.log.PanicIfError(err)
+			ts.log.PanicIfError(err)
 		}
 
 		// Update in-memory data
@@ -101,32 +109,32 @@ func (ts *table) writeToBuffer() {
 	}
 }
 
-// flushToFile writes the buffered data to disk
-func (ts *table) flushToFile() {
+// FlushToFile writes the buffered data to disk
+func (ts *Table) FlushToFile() {
 	if ts.Step == -1 {
 		return
 	}
 	// Check if the table state has changed
 	currentHash := ts.generateHash()
 	if currentHash == ts.lastSavedHash {
-		ts.e.log.Debug("Table state has not changed, skipping save.")
+		ts.log.Debug("Table state has not changed, skipping save.")
 		return
 	}
 	for i := range ts.columns {
 		// Update zarray if necessary, it is not buffered at the moment
-		err := ts.e.fs.SaveFileTableZarray("table/"+ts.columns[i].name, ts.Step)
+		err := ts.fs.SaveFileTableZarray("table/"+ts.columns[i].name, ts.Step)
 		if err != nil {
-			ts.e.log.PanicIfError(err)
+			ts.log.PanicIfError(err)
 		}
 		err = ts.columns[i].writer.Flush()
 		if err != nil {
-			ts.e.log.PanicIfError(err)
+			ts.log.PanicIfError(err)
 		}
 	}
 	ts.lastSavedHash = currentHash
 }
 
-func (ts *table) exists(q quantity.Quantity, name string) bool {
+func (ts *Table) exists(q quantity.Quantity, name string) bool {
 	suffixes := []string{"x", "y", "z"}
 	for _, col := range ts.columns {
 		if q.NComp() == 1 {
@@ -144,40 +152,40 @@ func (ts *table) exists(q quantity.Quantity, name string) bool {
 	return false
 }
 
-func (ts *table) addColumn(name, unit string) {
+func (ts *Table) addColumn(name, unit string) {
 	if !ts.initialized {
 		ts.initTable()
 	}
-	err := ts.e.fs.Mkdir("table/" + name)
-	ts.e.log.PanicIfError(err)
-	writer, file, err := ts.e.fs.Create("table/" + name + "/0")
-	ts.e.log.PanicIfError(err)
+	err := ts.fs.Mkdir("table/" + name)
+	ts.log.PanicIfError(err)
+	writer, file, err := ts.fs.Create("table/" + name + "/0")
+	ts.log.PanicIfError(err)
 	ts.columns = append(ts.columns, column{name: name, unit: unit, writer: writer, file: file})
 }
 
-func (ts *table) tableSave() {
+func (ts *Table) tableSave() {
 	if len(ts.columns) == 0 {
-		ts.e.log.Warn("No columns in table, not saving.")
+		ts.log.Warn("No columns in table, not saving.")
 	}
 	ts.Step += 1
 	ts.writeToBuffer()
 }
 
-func (ts *table) tableAdd(q quantity.Quantity) {
+func (ts *Table) tableAdd(q quantity.Quantity) {
 	ts.tableAddAs(q, q.Name())
 }
 
-func (ts *table) tableAddAs(q quantity.Quantity, name string) {
+func (ts *Table) tableAddAs(q quantity.Quantity, name string) {
 	suffixes := []string{"x", "y", "z"}
 	if ts.Step != -1 {
-		ts.e.log.Warn("You cannot add a new quantity to the table after the simulation has started. Ignoring.")
+		ts.log.Warn("You cannot add a new quantity to the table after the simulation has started. Ignoring.")
 	}
 	if len(ts.columns) == 0 {
-		ts.e.log.Warn("No columns in table, not saving.")
+		ts.log.Warn("No columns in table, not saving.")
 	}
 
 	if ts.exists(q, name) {
-		ts.e.log.Warn("%s is already in the table. Ignoring.", name)
+		ts.log.Warn("%s is already in the table. Ignoring.", name)
 		return
 	}
 	ts.quantities = append(ts.quantities, q)
@@ -190,16 +198,16 @@ func (ts *table) tableAddAs(q quantity.Quantity, name string) {
 	}
 }
 
-func (ts *table) tableAutoSave(period float64) {
-	ts.AutoSaveStart = ts.e.solver.Time
+func (ts *Table) tableAutoSave(period float64) {
+	ts.AutoSaveStart = ts.solver.Time
 	ts.AutoSavePeriod = period
 }
 
-func (ts *table) close() {
+func (ts *Table) Close() {
 	for _, col := range ts.columns {
 		err := col.writer.Flush()
-		ts.e.log.PanicIfError(err)
+		ts.log.PanicIfError(err)
 		err = col.file.Close()
-		ts.e.log.PanicIfError(err)
+		ts.log.PanicIfError(err)
 	}
 }
